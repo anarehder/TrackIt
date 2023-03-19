8642ed2db5a322c367a06098fec08b8e56aacec4
anarehder <anapaula.rehder@gmail.com>
2023-03-19 20:08:31 -0300

    Tela Hoje

--- src/components/HabitoDiario/HabitoDiario.js
+++ src/components/HabitoDiario/HabitoDiario.js
@@ -1,9 +1,8 @@
 import { ContainerHabitoDiario, Texto, Icone, Titulo, Atual, Recorde } from "./styled"
 import done from "../../assets/done.png"
-import { useState } from "react";
 
 export default function HabitoDiario({ name, id, atual, feito, recorde, marcarHabito }) {
-
+    console.log(atual, recorde);
     function marcar(id) {
         const body = {};
         console.log("marquei", id);
@@ -15,10 +14,10 @@ export default function HabitoDiario({ name, id, atual, feito, recorde, marcarHa
             <Texto>
                 <Titulo data-test="today-habit-name">{name}</Titulo>
                 <p data-test="today-habit-sequence" >
-                    Sequência atual: <Atual status={feito === true ? "concluido" : ""}>{atual} dias</Atual>
+                    Sequência atual: <Atual status={feito === true || (recorde === atual && atual > 0 )? "concluido" : ""}>{atual} dias</Atual>
                 </p>
                 <p data-test="today-habit-record">
-                    Seu recorde: <Recorde status={recorde > atual || recorde === 0 ? "" : "concluido"}>{recorde} dias</Recorde>
+                    Seu recorde: <Recorde status={recorde === atual && recorde > 0 ? "concluido" : ""}>{recorde} dias</Recorde>
                 </p>
             </Texto>
             <Icone status={feito === true ? "concluido" : ""} onClick={() => marcar(id, feito)}>
